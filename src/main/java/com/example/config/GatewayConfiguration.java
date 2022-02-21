7d08ee65d81212be38e15920377ac7aed94c2b1b
dejvis06 <dejvisbeqiraj06@gmail.com>
2022-02-21 11:25:48 +0100

    Added RouteLocator;

---
+++ src/main/java/com/example/config/GatewayConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(filter -> filter
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
+                .build();
+    }
+}
